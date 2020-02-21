b5eab95ce1a8245c2e44ba5a76aedaabb216902b
Brandon Black <brandonblack02@gmail.com>
2020-02-21 13:17:37 -0800

    Back from Apple support call, added form values

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-var q1_value = document.getElementById("firstQuestion");
-console.log('Hello!');
+let q1_value = 0;
+
+
+let q2_value = 0;
+let a1 = document.getElementById("check1");
+let a2 = document.getElementById("check2");
+let a3 = document.getElementById("check3");
+let arr = [a1, a2, a3]
+
+
+
+document.defaultView.addEventListener('click', () => {
+    q1_value = parseInt(document.getElementById("firstQuestion").first.value);
+    if (q1_value === 100) {
+        document.getElementById("secondQuestion").classList.remove("no-show");
+    } else {
+        document.getElementById("secondQuestion").classList.add("no-show");
+    }
+    q2_value = 0
+    arr.forEach( (item) => {
+        if (item.checked) {
+            q2_value += parseInt(item.value);
+        }
+    })
+    console.log(q2_value, q1_value);
+})

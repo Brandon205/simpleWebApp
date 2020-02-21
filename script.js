210366b9681b7c39719d2ba7dde83f5891ec8427
Brandon Black <brandonblack02@gmail.com>
2020-02-21 13:59:23 -0800

    Final touches put on app

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 let q1_value = 0;
-
-
 let q2_value = 0;
+let sum;
 let a1 = document.getElementById("check1");
 let a2 = document.getElementById("check2");
 let a3 = document.getElementById("check3");
-let arr = [a1, a2, a3]
-
-
+let arr = [a1, a2, a3];
 
 document.defaultView.addEventListener('click', () => {
     q1_value = parseInt(document.getElementById("firstQuestion").first.value);
     if (q1_value === 100) {
         document.getElementById("secondQuestion").classList.remove("no-show");
+        sum = q1_value + q2_value;
     } else {
         document.getElementById("secondQuestion").classList.add("no-show");
+        sum = q1_value;
     }
     q2_value = 0
     arr.forEach( (item) => {
@@ -22,5 +21,13 @@ document.defaultView.addEventListener('click', () => {
             q2_value += parseInt(item.value);
         }
     })
-    console.log(q2_value, q1_value);
+})
+
+document.getElementById("submit").addEventListener('click', () => {
+    event.preventDefault();
+    // Here is where we would finish the validations that haven't yet been validated and then send the data to the API (SendToServer)
+});
+
+document.getElementById("skip").addEventListener('click', () => {
+    location.reload();
 })
